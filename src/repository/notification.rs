2524c95f0eb117c7028a241a8f57f42bf2f71cfb
zufarra <zufarromli@gmail.com>
2025-03-27 12:42:43 +0700

    fix error on lazy_static variable in Subscriber Repository.

--- src/repository/notification.rs
+++ src/repository/notification.rs
@@ -5,9 +5,9 @@ use lazy_static::lazy_static;
 use crate::model::notification::Notification;
 
 // Singleton of Database
-lazy_static!(
+lazy_static! {
     static ref NOTIFICATIONS: RwLock<Vec<Notification>> = RwLock::new(vec![]);
-)
+}
 
 pub struct NotificationRepository;
 
